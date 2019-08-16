4931c88239442ff10b74a6cfb7150f0b94c4ec6b
vagrant <vagrant@vagrant.vm>
2019-08-16 16:02:49 +0000

    reworked to optimize clarifai code to run multiple training sets

--- clarifai_test/poc_clarifai.py
+++ clarifai_test/poc_clarifai.py
@@ -3,43 +3,51 @@ import os
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 import json
-from pprint import pprint
+
 
 CLARIFAI_TOKEN= os.environ.get('CLARIFAI_TOKEN')
-print(CLARIFAI_TOKEN)
-print(CLARIFAI_TOKEN)
 app = ClarifaiApp(api_key=CLARIFAI_TOKEN)
 
 
-# create empty image lists
-smooth_abst_images=[]
-serrated_abst_images=[]
+def train_images():
+    """train images"""
 
-# Iterate through folder with test images and add to designated concept
-for file in os.listdir('img/shapes/smooth/abstract'):
-   img=ClImage( filename=f'img/shapes/smooth/abstract/{file}', concepts=['smooth_abst1'])
-   smooth_abst_images.append(img)
+    # create empty image lists
+    platanus_images=[]
+    magnolia_images=[]
+    prunus_images=[]
+    tristan_images=[]
+    ficus_images=[]
 
-for file in os.listdir('img/shapes/serrated/abstract'):
-    img = ClImage(filename=f'img/shapes/serrated/abstract/{file}', concepts=['serrated_abst1'])
-    serrated_abst_images.append(img)
+    test_trees = ['platanus','magnolia','prunus','tristan','ficus']
+    test_images = [platanus_images,magnolia_images,prunus_images,tristan_images,ficus_images]
+    
+    # Iterate through folder with test images and add to designated concept    
+    for i,tree in enumerate(test_trees):
+        for file in os.listdir('img/{tree}'):
+           img=ClImage( filename=f'img/{tree}/{file}', concepts=[{tree}])
+           test_images[i].append(img)
 
-# Bulk create images
-app.inputs.bulk_create_images(smooth_abst_images)
-app.inputs.bulk_create_images(serrated_abst_images)
 
+        # Bulk create images
+        app.inputs.bulk_create_images(test_images[i])
 
-# Create model and add chosen concepts to model
-model=app.models.create('test_edge_g', concepts =['smooth_abst1','serrated_abst1'])
+def make_prediction(user_picture, trees=['platanus','magnolia','prunus','tristan','ficus']):
+    """make prediction """
 
-# train the model
-model.train()
+    train_images()
+    # Create model and add chosen concepts to model
+    model=app.models.create('detect_tree', concepts = trees)
 
-# RUN PREDICTION
-    # Value of 0 means object was NOT detected. 
-    # Value above 1 means 100% certainty object was detected.
-prediction = model.predict_by_filename('img/shapes/single_smooth_outdoors2.jpg')
+    # train the model
+    model.train()
 
+    # RUN PREDICTION
+        # Value of 0 means object was NOT detected. 
+        # Value above 1 means 100% certainty object was detected.
+    prediction = model.predict_by_filename('img/test_leaf/{user_picture}')
+
+    # delete model
+        #add code here#
  
-pprint(prediction)
 
