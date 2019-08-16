2c6f6a7f8e835a9c7d1b188e0f4d271997df50f3
vagrant <vagrant@vagrant.vm>
2019-08-16 00:15:25 +0000

    removed dictionary component in seed.py. Renamed tables to TreeSpecies and Tree. Added relationship to Tree table to connect with TreeSpecies.

--- model.py
+++ model.py
@@ -4,44 +4,63 @@ db = SQLAlchemy()
 
 
 
-class Tree(db.Model):
+class TreeSpecies(db.Model):
     """ Trees """
-    __table__ = 'trees'
 
-    tree_id = db.Column( db.Integer, primary_key = True, autoincrement = True)
-    sci_name = db.Column(db.String(), nullable = False)
-    common_name = db.Column(db.String(), nullable = False)
-    # factoid = db.Column (db.String(), nullable= False)
-    # margin = db.Column(db.String(), nullable = False) 
-    # venation = db.Column(db.String(), nullable = True)
-    # image = db. Column(db.String(), nullable = False)
+    __tablename__ = 'tree_species'
+
+    tree_species_id = db.Column(db.Integer,
+                                primary_key=True,
+                                autoincrement=True)
+    sci_name = db.Column(db.String, nullable = False)
+    common_name = db.Column(db.String, nullable = False)
+    factoid = db.Column (db.String, nullable= False)
+    margin = db.Column(db.String, nullable = False) 
+    venation = db.Column(db.String, nullable = True)
+    image = db. Column(db.String, nullable = False)
     
     def __repr__(self):
         """show leaf name and main categories"""
 
-        return f"<Tree tree_id={self.tree_id} common_name={self.common_name} single_leaf={self.single_leaf}>"
+        return f"<TreeSpecies tree_id={self.tree_species_id} common_name={self.common_name}>"
 
     
-class Location(db.Model):
+class Tree(db.Model):
     """Data SF tree location"""
 
-    __table__ = 'location'
+    __tablename__ = 'trees'
 
-    lat = db.Column(db.Float, primary_key = True, nullable = False)
-    lon = db.Column(db.Float, primary_key = True, nullable = False)
-    tree_id = db.Column(db.Integer, db.ForiegnKey('trees'), primary_key = True)
+    tree_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True)
+    lat = db.Column(db.Float, nullable = False)
+    lon = db.Column(db.Float, nullable = False)
+    tree_species_id = db.Column(db.Integer,
+                                db.ForeignKey('tree_species.tree_species_id'))
+
+    tree_species = db.relationship('TreeSpecies', backref='trees')
 
     def __repr__(self):
         """ Show tree id an Lat/Lon """
 
-        return f"<Location tree_id={self.tree_id} lat={self.lat} lon={self.lon}>"
+        return f"<Tree tree_id={self.tree_id} lat={self.lat} lon={self.lon}>"
 
-db.create_all()
 
-def connect_to_db(app, db_name):
+
+def connect_to_db(app):
     """Connect to database"""
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///trees'
     app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+
+    print('Connected to db!')
+
+
+if __name__ == '__main__':
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
--- seed.py
+++ seed.py
@@ -1,86 +1,90 @@
-from model import connect_to_db, db
- 
-# TREE_DICT = {
-#                  'Platanus x hispanica ': {'common_name': 'Sycamore: London Plane', 'lat': '37.7776435713687', 'lon': '-122.416171839707'}, 
-#                  'Metrosideros excelsa ': {'common_name': 'New Zealand Xmas Tree', 'lat': '37.7159856948639', 'lon': '-122.478342532492'}, 
-#                  'Lophostemon confertus ': {'common_name': 'Brisbane Box', 'lat': '37.7662509163514', 'lon': '-122.464348446646'}, 
-#                  'Pittosporum undulatum ': {'common_name': 'Victorian Box', 'lat': '37.7639964041248', 'lon': '-122.468487066847'}, 
-#                  'Tristaniopsis laurina ': {'common_name': 'Swamp Myrtle', 'lat': '37.7309323858205', 'lon': '-122.436517937945'}, 
-#                  'Prunus cerasifera ': {'common_name': 'Cherry Plum', 'lat': '37.7390053040631', 'lon': '-122.424668946839'}, 
-#                  'Magnolia grandiflora ': {'common_name': 'Southern Magnolia', 'lat': '37.7577074184489', 'lon': '-122.384568876102'}, 
-#                  "Ficus microcarpa nitida 'Green Gem' ": {'common_name': "Indian Laurel Fig Tree 'Green Gem'", 'lat': '37.777476320014', 'lon': '-122.404479121929'}, 
-#                  "Arbutus 'Marina' ": {'common_name': 'Hybrid Strawberry Tree', 'lat': '37.7523340257173', 'lon': '-122.473048745206'}, 
-#                  "Prunus serrulata 'Kwanzan' ": {'common_name': 'Kwanzan Flowering Cherry', 'lat': '37.7552838973381', 'lon': '-122.469226308173'}
-#             }
-
 
 import json
 from pprint import pprint
 
 TREE_DATA = "trees_sf/rows.json"
+
 trees_json = open(TREE_DATA).read()
 
 tree_info = json.loads(trees_json)
 
-# entries = tree_info['data'].__len__()
+entries = tree_info['data'].__len__()
 
 # create an empty TREE_TYPE_DICT
-TREE_DICT = {}
-for info in tree_info:
+TREE_LIST = []
+i=0
+
+while i < entries:
+
     tree_type =tree_info['data'][i][10]
+
       # split out the scientific and common name from data
     scientific_name, common_name = tree_type.split('::')
-    scientific = scientific_name.rstrip()
-    common_name = common_name.lstrip() 
+    scientific_name = scientific_name.strip()
+    common_name = common_name.strip()
 
-        if scientific_name in TREE_DICT:
-            count = 1
-        if scientific_name == 'Platanus x hispanica ':
-            shape = 'palmate'
-            margin = 'entire'
-            venation = 'palmate'
-            factiod = 'Dicidous Tree. Bare through November-March. Member of the Sycamore Family'
-            image = 'static\img\platanus_x_hispanica.jpg'   
+    latitude = tree_info['data'][i][23]
+    longitude = tree_info['data'][i][24] 
 
-    # extract scientific name
-        #if name is new
-            # add an additional key count:0
-        #if name is not new
-            # incriment count by one
-        # if name is Platanus x hispanica
-            # add  this additional information
-                # shape: palmate
-                # margin : entire
-                # venation : pinnate
-                # factoid : Dicidous Tree. Bare through November-March. Member of the Sycamore Family.
-                # image: 'img\platanus_x_hispanica.jpg'
-        # if name is Magnolia Grandiflora
-            # add this additional information
-                # shape = obtuse
-                # margin = entire
-                # venation = 
-    # extract common name 
+   
+    if latitude == None and longitude==None:
+        pass
+        
+    if tree_type == 'Tree(s) ::':
+        pass
 
-    # add to TREE_DICT[scientific names] = {common name, shape, margin, venation}
+    if scientific_name == 'Platanus x hispanica ':
+        shape = 'palmate'
+        margin = 'entire'
+        venation = 'pinnate'
+        factiod = 'Dicidous Tree. Bare through November-March. Member of the Sycamore Family'
+        image = '/static/img/platanus_x_hispanica.jpg'
+
+    if scientific_name == 'Magnolia grandiflora ':
+        shape = 'obtuse'
+        margin = 'entire'
+        venation = 'pinnate'
+        factiod = ''
+        image = ''
+
+    if scientific_name == 'Prunus cerasifera ':
+        shape = 'obtuse'
+        margin = 'serrated'
+        venation = ''
+        factiod = ''
+        image = ''
+
+    if scientific_name == 'Tristaniopsis laurina ':
+        shape = ''
+        margin = ''
+        venation = ''
+        factiod = ''
+        image = ''
+        
+    if scientific_name == "Ficus microcarpa nitida 'Green Gem' ":
+        shape = 'obtuse'
+        margin = 'entire'
+        venation = 'pinnate'
+        factiod = ''
+        image = ''
+
+    i+=1
+
+    species =TreeSpecies(
+            sci_name = scientific_name, 
+            common_name = common_name, 
+            shape= shape, 
+            margin = margin, 
+            venation = venation, 
+            factoid = factoid,
+            image = image)
+
+    tree = Tree(lat= lat, lon= lon)
 
 
-for data in TREE_DICT:
-    data = Tree(sci_name = data, common_name = data['common_name'])
+# if __name__=="__main__":
+#     connect_to_db(app)
 
-#create TREE_LOCATION_DICT
-
-# iterate through json file
-    # latitude = tree_info['data'][i][23]
-    # longitude = tree_info['data'][i][24]
-    # add data to Location table
-
-
-for data in TREE_DICT:
-    data = Location(lat=data['lat'], lon = data['lon'])
-
-if __name__=="__main__":
-    connect_to_db(app)
-
-    db.create_all()
+#     db.create_all()
 
 
